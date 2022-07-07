14c064cf2563b6731de889c72fe4bf89344fa572
Andres Felipe Brieva Pinedo <53844420+BladeNigth@users.noreply.github.com>
2022-07-06 23:00:59 -0500

    Terminando Ejercicio

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,11 @@ public class Main {
         int num2 = 4;
         int num3 = 8;
         int result = suma(num1,num2,num3);
+        System.out.println("La suma de " + num1 + " + "+  num2 + " + " + num3 + " es: " + result);
 
+        coche micoche = new coche();
+        micoche.miCoche();
+        System.out.println("El numero de puertas es: " + micoche.getNumpuertas());
     }
 
     public static int suma(int a, int b, int c){
@@ -15,7 +19,7 @@ public class Main {
     public static class coche {
         public static int numpuertas = 4;
 
-        public static void miChoche(){
+        public static void miCoche(){
             numpuertas += 1;
         }
 
